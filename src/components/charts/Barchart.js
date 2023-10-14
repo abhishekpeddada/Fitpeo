75d06507356b37261443f549502668423e3a426d
abhishekpeddada <abhishekpeddada@gmail.com>
2023-10-14 12:05:54 +0530

    Add more content to barchart

--- src/components/charts/Barchart.js
+++ src/components/charts/Barchart.js
@@ -6,13 +6,24 @@ const Barchart = () => {
     const labels = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
     return (
-        <div className="bar-chart">
-            {data.map((value, index) => (
-                <div key={index} className="bar-container">
-                    <div className="bar" style={{ height: `${value}px` }}></div>
-                    <span className="label">{labels[index]}</span>
+        <div className="barchart-container">
+            <div className="barchart-header">
+                <div>
+                <h2>Overview</h2>
+                <p className='earnings'>Monthly Earnings</p>
                 </div>
-            ))}
+                <select className="selector">
+                    <option value="quarterly">Quarterly</option>
+                </select>
+            </div>
+            <div className="bar-chart">
+                {data.map((value, index) => (
+                    <div key={index} className="bar-container">
+                        <div className="bar" style={{ height: `${value}px` }}></div>
+                        <span className="label">{labels[index]}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
