c7000118e428f60c708a95d354a19800d0aacf41
abhishekpeddada <abhishekpeddada@gmail.com>
2023-10-14 09:37:48 +0530

    Fix sidebar visibility

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../../src/styles/styles.css';
 
 function Sidebar() {
     const [showSidebar, setShowSidebar] = useState(true);
     const [hamburgerClass, setHamburgerClass] = useState('hamburger');
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 600px)");
+        setShowSidebar(!mediaQuery.matches);
+        setHamburgerClass(mediaQuery.matches ? 'hamburger-min' : 'hamburger');
+
+        const handleChange = (e) => {
+            setShowSidebar(!e.matches);
+            setHamburgerClass(e.matches ? 'hamburger-min' : 'hamburger');
+        };
+
+        mediaQuery.addListener(handleChange);
+        return () => mediaQuery.removeListener(handleChange);
+    }, []);
 
     const toggleSidebar = () => {
         setShowSidebar(!showSidebar);
@@ -29,25 +43,23 @@ function Sidebar() {
             <div className={hamburgerClass} onClick={toggleSidebar}>
                 &#9776;
             </div>
-            {showSidebar && (
-                <div className="Sidebar">
-                    <div className="logo">Dashboard</div>
-                    <ul className="menu">
-                        {menuItems.map((item) => (
-                            <li
-                                className={selectedItem === item.id ? 'selected' : ''}
-                                onClick={() => handleItemClick(item.id)}
-                                key={item.id}
-                            >
-                                <a href="#">
-                                    {item.name}
-                                </a>
-                            </li>
-                        ))}
-                        <li className='profile' ><a>Ivano</a><span className='product-manager'>Product Manager</span></li>
-                    </ul>
-                </div>
-            )}
+            <div className={showSidebar ? "Sidebar" : "Sidebar hidden"}>
+                <div className="logo">Dashboard</div>
+                <ul className="menu">
+                    {menuItems.map((item) => (
+                        <li
+                            className={selectedItem === item.id ? 'selected' : ''}
+                            onClick={() => handleItemClick(item.id)}
+                            key={item.id}
+                        >
+                            <a href="#">
+                                {item.name}
+                            </a>
+                        </li>
+                    ))}
+                    <li className='profile' ><a>Ivano</a><span className='product-manager'>Product Manager</span></li>
+                </ul>
+            </div>
         </div>
     );
 }
